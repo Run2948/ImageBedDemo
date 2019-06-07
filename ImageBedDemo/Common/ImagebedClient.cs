8121f379f46e0a694196b293a6836bfb7fe14c57
少年那已不再 <15997352948@163.com>
2019-06-07 12:21:55 +0800

    添加项目文件。

---
+++ ImageBedDemo/Common/ImagebedClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageBedDemo.Common
+{
+    /// <summary>
+    /// 图床客户端
+    /// </summary>
+    public class ImagebedClient
+    {
+    }
+}
